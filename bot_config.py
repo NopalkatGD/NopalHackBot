60ed2065afebfe783601dcc32426c37429534b2a
NopalHackBot <dvd.7435@gmail.com>
2025-10-21 10:21:38 -0600

    evitar reinicio 20 10 2025 20 21

--- bot_config.py
+++ bot_config.py
@@ -124,4 +124,4 @@ def home():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, use_reloader=False)
